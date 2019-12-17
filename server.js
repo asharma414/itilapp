ba7bce9ee8b28e3bd6db52b96b54378dee366ce5
Abhinav Sharma <Abhinav@Dellintosh.local>
2019-12-17 02:16:58 -0500

    more fixes

--- server.js
+++ server.js
@@ -25,10 +25,10 @@ require('./passport')(passport);
 
 if(process.env.NODE_DEV === 'production' || process.env.NODE_DEV === 'staging') {
     // set static folder
-    app.use(express.static('./client/build'));
+    app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, './client/build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
