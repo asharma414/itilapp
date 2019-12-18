2042147a9cc03daa53cc4d131a14e9fd077cc3f3
Abhinav Sharma <Abhinav@Dellintosh.local>
2019-12-18 00:58:41 -0500

    heroku testing

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
-const db = process.env.MONGOURI || require('./keys').mongoURI;
+const db = process.env.MONGOURI || 'mongodb+srv://test:test@cluster0-go8ql.mongodb.net/itilapp?retryWrites=true&w=majority';
 const port = process.env.PORT || 5000;
 const passport = require('passport');
 const userRoutes = require('./routes/users');
