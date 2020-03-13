65b18205d86f30050ab1b1e11dc1c0874da12eb4
Abhinav Sharma <Abhinav@Dellintosh.local>
2020-03-13 02:00:23 -0400

    heroku testing

--- server.js
+++ server.js
@@ -31,9 +31,10 @@ app.use('/api/tickets', ticketRoutes);
 if(process.env.NODE_ENV === 'production') {
     // set static folder
     app.use(express.static('client/build'));
-    
+
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
 
 app.listen(port, () => {
