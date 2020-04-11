30d5945707e908c43e77aa72762ee5246fdf74ef
Abhinav Sharma <Abhinav@Dellintosh.local>
2020-04-11 02:25:22 -0400

    autoclose debugging

--- models/ticketResolve.model.js
+++ models/ticketResolve.model.js
@@ -2,10 +2,9 @@ const mongoose = require('mongoose');
 
 
 const resolveSchema = new mongoose.Schema({
-    createdAt: { type: Date, default: Date.now },
-    close_ref: { type: mongoose.Schema.Types.ObjectId, ref: 'Ticket'}
+    close_ref: { type: mongoose.Schema.Types.ObjectId, ref: 'Ticket'},
+    expireAt: { type: Date, expires: 300, default: Date.now }
 });
 
-resolveSchema.index({ createdAt: 1}, { expireAfterSeconds: 259200 })
 
 module.exports = mongoose.model('TicketResolve', resolveSchema)
