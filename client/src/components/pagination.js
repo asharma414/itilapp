281ae41263391ba89e990ad30aac300d609c18d7
Abhinav Sharma <Abhinav@Dellintosh.local>
2020-04-19 02:42:17 -0400

    favicon

--- client/src/components/pagination.js
+++ client/src/components/pagination.js
@@ -17,14 +17,14 @@ export default function Pagination({ ticketsPerPage, totalTickets, currentPage, 
         return (
             <nav>
                 <ul className='pagination'>
-                <li className={ currentPage == 1 ? 'd-none' : 'page-item' }><a onClick={() => navigate(--currentPage)} className='page-link'>Prev</a></li>
+                <li className={ currentPage === 1 ? 'd-none' : 'page-item' }><a onClick={() => navigate(--currentPage)} className='page-link'>Prev</a></li>
                 {pageNumbers.map(number => (
-                    <li key={number} className={ number == currentPage ? 'page-item active' : 'page-item'}>
+                    <li key={number} className={ number === currentPage ? 'page-item active' : 'page-item'}>
                         <a onClick={() => navigate(number)} className='page-link'>{number}</a>
                     </li>
                 ))}
                 {/* I had to use the paginate function for the next button to work. But it double decrements if I use paginate on previous button */}
-                <li className={ currentPage == pageNumbers.length ? 'd-none' : 'page-item' }><a onClick={() => navigate(++currentPage)} className='page-link'>Next</a></li>
+                <li className={ currentPage === pageNumbers.length ? 'd-none' : 'page-item' }><a onClick={() => navigate(++currentPage)} className='page-link'>Next</a></li>
                 </ul>
             </nav>
         )
